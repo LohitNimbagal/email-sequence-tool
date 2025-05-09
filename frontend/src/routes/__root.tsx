7660bc2d1290595896491b0ba1ab25c078da5e1e
LohitNimbagal <nimbagallohit@gmail.com>
2025-05-10 00:15:08 +0530

    update font-family

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,6 +4,8 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
     component: () => (
-        <Outlet />
+        <div className='font-inter'>
+            <Outlet />
+        </div>
     ),
 })
